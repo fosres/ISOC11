6799dcad1331209a61655060e336f6ae82ae7583
Tanveer Salim <tanveerasalim@gitlab.com>
2019-03-16 02:57:26 -0500

    master

--- first_quine.c
+++ first_quine.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
-
+static char arr[256];
 int main(void)
 {
 	rsize_t i = 0;
-	rsize_t opening = 7;
-	char * q[] = 
+
+	rsize_t opening = 9;
+	
+	char * q[] =
 	{
 "#include <stdio.h>",
 "",
 "int main(void)",
 "{",
 "	rsize_t i = 0;",
-"	rsize_t opening = 7;",
-"	char * q[] = ",
+"",
+"	rsize_t opening = 9;",
+"	",
+"	char * q[] =",
 "	{",
 "	};",
 "	while ( i <= opening )",
 "	{",
-"		puts(q[i]);",
-"		",
+"		printf(q[i]);",
+"		putchar(10);",
 "		i++;",
 "	}",
 "	i = 0;",
@@ -31,22 +35,23 @@ int main(void)
 "		putchar(10);",
 "		i++;",
 "	}",
+"	putchar(78);putchar(85);putchar(76);putchar(76);",
+"	putchar(10);",
 "	i = opening+1;",
 "	while ( q[i] != NULL )",
 "	{",
-"		puts(q[i]);",
-"",
+"		printf(q[i]);",
+"		putchar(10);",
 "		i++;",
 "	}",
-"	",
 "	return 0;",
 "}",
 NULL
 	};
 	while ( i <= opening )
 	{
-		puts(q[i]);
-		
+		printf(q[i]);
+		putchar(10);
 		i++;
 	}
 	i = 0;
@@ -59,15 +64,17 @@ NULL
 		putchar(10);
 		i++;
 	}
-	printf("NULL");
+	putchar(78);putchar(85);putchar(76);putchar(76);
 	putchar(10);
 	i = opening+1;
 	while ( q[i] != NULL )
 	{
-		puts(q[i]);
-
+		printf(q[i]);
+		putchar(10);
 		i++;
 	}
 
 	return 0;
+
+
 }
