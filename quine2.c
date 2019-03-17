1c41c288b9ff2f41e3903da1174dbf0baf06704d
Tanveer Salim <tanveerasalim@github.com>
2019-03-16 21:21:08 -0500

    master

---
+++ quine2.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+ int main(void){ char *s = "#include <stdio.h>
+ int main(void){ char *s = %c%s%c; printf(s,34,s,34); return 0;"; printf(s,34,s,34); return 0;
---
+++ third_quine.c
@@ -0,0 +1,2 @@
+#include <stdio.h> 
+int main(void){ char *s = "#include <stdio.h>\n int main(void){ char *s = %c%s%c; printf(s,34,s,34); return 0;"; printf(s,34,s,34); return 0; }
