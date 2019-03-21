e18e3d30677245c933b87e4dd73d818d42c6c4b1
Tanveer Salim <tanveerasalim@github.com>
2019-03-21 03:45:09 -0500

    master

--- ken_quine_main_end.c
+++ ken_quine_main_end.c
@@ -1,12 +1,15 @@
 
-FILE * target = NULL;
+void generate_target_file(void)
+{
+
+FILE * target_file = NULL;
 
 static char name[1024];  
 
 snprintf(name,"%s\b\b_quine.c\0",argv[1]);
 
 
-if ( ( target = fopen(name,"wb+") ) == NULL )
+if ( ( target_file = fopen(name,"wb+") ) == NULL )
 {
 	
 	fprintf(stdout,"%d: Error! Can't open %s for rewriting!\n",name);
@@ -20,15 +23,15 @@ static char * q_p = q;
 
 while ( *q_p != 0x0 )
 {
-	fputc(target_file.c,*q_p);
+	fputc(target_file,*q_p);
 
 	q_p++;
 }
 
 
-fprintf("%s\n",q,target_file.c);
+fprintf(target_file,"%s\n",q);
 
-if ( fclose(target)  == EOF )
+if ( fclose(target_file)  == EOF )
 {
 	fprintf(stdout,"Error! Failed to close %s!\n",name);
 
@@ -36,4 +39,4 @@ if ( fclose(target)  == EOF )
 
 }
 
-
+}
---
+++ stack_size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+#include "char_arr.h"
+
+int main()
+{
+	printf("%s",q);	
+	return 0;
+}
