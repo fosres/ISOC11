b83649ef02e463677f860a95308b7e8d0e677729
Tanveer Salim <tanveerasalim@github.com>
2019-03-20 23:12:32 -0500

    master

--- ken_quine_generator.c
+++ ken_quine_generator.c
@@ -6,10 +6,36 @@ to designing a simple quine program:
 
 http://www.azillionmonkeys.com/qed/self3_c.txt
 
-To convert any non-quine program into a quine program, simply write:
+To convert any non-quine program into a quine program, simply write something
+
+like (leave the empty lines as is when adding them to ken_quine_generator.c:
+
+FILE * target = NULL;
+
+static char name[1024];  
+
+snprintf(name,"%s\b\b_quine.c\0",argv[1]);
 
 
-printf("%s\n",q);
+if ( ( target = fopen(name,"wb+") ) == NULL )
+{
+	
+	fprintf(stdout,"%d: Error! Can't open %s for rewriting!\n",name);
+}
+
+
+
+static char * q_p = q;
+
+while ( *q_p != 0x0 )
+{
+	fputc(target_file.c,*q_p);
+
+	q_p++;
+}
+
+
+fprintf("%s\n",q,target_file.c);
 
 RIGHT BEFORE:
 
